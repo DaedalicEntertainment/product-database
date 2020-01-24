12110895ee0169c78132ab000b28e09ff1ec2939
daedalic-npruehs <nick.pruehs@daedalic.com>
2020-01-24 15:06:38 +0100

    Seed database with initial data.

---
+++ Source/ProductDatabase/Daedalic.ProductDatabase/Models/SeedData.cs
@@ -0,0 +1,35 @@
+﻿using Daedalic.ProductDatabase.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Daedalic.ProductDatabase.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DaedalicProductDatabaseContext
+                (serviceProvider.GetRequiredService<DbContextOptions<DaedalicProductDatabaseContext>>()))
+            {
+                if (context.Game.Any())
+                {
+                    // DB already seeded.
+                    return;   
+                }
+
+                context.Game.AddRange(
+                    new Game
+                    {
+                        Name = "Anna's Quest"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
