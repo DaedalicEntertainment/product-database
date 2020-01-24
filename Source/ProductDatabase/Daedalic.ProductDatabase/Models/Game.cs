77048e78abb0f0bd9734551c0f6763dc9ff02645
daedalic-npruehs <nick.pruehs@daedalic.com>
2020-01-24 15:32:01 +0100

    Validate game names.

--- Source/ProductDatabase/Daedalic.ProductDatabase/Models/Game.cs
+++ Source/ProductDatabase/Daedalic.ProductDatabase/Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace Daedalic.ProductDatabase.Models
     {
         public int Id { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
     }
 }
