d94d418b8c6d7e9b6a9ccdb7c39c7a9b34d5ee32
daedalic-npruehs <nick.pruehs@daedalic.com>
2020-01-27 07:21:15 +0100

    Get asset index URL from configuration.

--- Source/ProductDatabase/Daedalic.ProductDatabase/Pages/Games/Index.cshtml.cs
+++ Source/ProductDatabase/Daedalic.ProductDatabase/Pages/Games/Index.cshtml.cs
@@ -7,19 +7,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Daedalic.ProductDatabase.Data;
 using Daedalic.ProductDatabase.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Daedalic.ProductDatabase
 {
     public class IndexModel : PageModel
     {
         private readonly Daedalic.ProductDatabase.Data.DaedalicProductDatabaseContext _context;
+        private readonly IConfiguration _config;
 
-        public IndexModel(Daedalic.ProductDatabase.Data.DaedalicProductDatabaseContext context)
+        public IndexModel(Daedalic.ProductDatabase.Data.DaedalicProductDatabaseContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         public IList<Game> Game { get;set; }
+
+        public string AssetIndexUrl { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string Filter { get; set; }
@@ -33,6 +38,8 @@ namespace Daedalic.ProductDatabase
             }
 
             Game = await games.ToListAsync();
+
+            AssetIndexUrl = _config.GetValue<string>("AssetIndexUrl");
         }
     }
 }
