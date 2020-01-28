0650745e3527e12b9be56ad189d6005fb257fb95
daedalic-npruehs <nick.pruehs@daedalic.com>
2020-01-28 09:20:35 +0100

    Add more data to welcome page.

--- Source/ProductDatabase/Daedalic.ProductDatabase/Pages/Index.cshtml.cs
+++ Source/ProductDatabase/Daedalic.ProductDatabase/Pages/Index.cshtml.cs
@@ -10,16 +10,39 @@ namespace Daedalic.ProductDatabase.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly Daedalic.ProductDatabase.Data.DaedalicProductDatabaseContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(Daedalic.ProductDatabase.Data.DaedalicProductDatabaseContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+
+        public int Developers { get; set; }
+
+        public int Games { get; set; }
+
+        public int Genres { get; set; }
+
+        public int Languages { get; set; }
+
+        public int Platforms { get; set; }
+
+        public int Publishers { get; set; }
+
+        public int Releases { get; set; }
+
+        public int Stores { get; set; }
 
         public void OnGet()
         {
-
+            Developers = _context.Developer.Count();
+            Games = _context.Game.Count();
+            Genres = _context.Genre.Count();
+            Languages = _context.Language.Count();
+            Platforms = _context.Platform.Count();
+            Publishers = _context.Publisher.Count();
+            Releases = _context.Release.Count();
+            Stores = _context.Store.Count();
         }
     }
 }
